eaeb2e648ddb3d51fd87d7355581cebfcfd2b661
Sara Tisell <Sara.Tisell@hotmail.com>
2023-11-28 16:08:33 +0000

    Add a 'welcome letter', list and function for dormitories

--- run.py
+++ run.py
@@ -1,10 +1,22 @@
-# Description start game
+# Description for game
 print("Welcome to Hogwarts Adventure")
 print("If you are ready to play the game please enter your name")
 name = input()
 print("Thank you, lets play and have fun!")
 print()
-print(f"Dear, {name}\nYou have been accepted to Hogwarts School for Witchcraft and Wizardry")
-print("It is time to place you in the right dormitory, our four dormitories are:\nGriffindor\nRavenclaw\nHufflepuff\nand\nSlytherin")
+
+"""
+Start game
+Welcome letter
+"""
+print(f"Dear, {name}\nYou have been accepted to Hogwarts School for Witchcraft and Wizardry!")
+print("It is time to place you in the right dormitory, our four dormitories are:")
+dormitories = ["Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"]
+print(*dormitories, sep="\n")
+
+def select_dormitory():
+    
+
+
 
 # Dormitories options
