03b41c1ba0f8a1b21a05b78f1e815f8ca7273627
Sara Tisell <Sara.Tisell@hotmail.com>
2023-11-28 16:37:53 +0000

    Add functions for game path

--- run.py
+++ run.py
@@ -14,10 +14,36 @@ print("It is time to place you in the right dormitory, our four dormitories are:
 dormitories = ["Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"]
 print(*dormitories, sep="\n")
 
+# Select Dormitory
 def select_dormitory():
     print("We need to know your strenghts to make sure to put you in the right dormitory.\nPlease choose one of these four skills that best describes you greates strength! ")
     dormitories_strengths = ["Bravery", "Curiosity", "Loyalty", "Ambition"]
+    user_dormitory_strenght = input()
+   # dormitory = input()
+
+# Challenge description
+
+print(f"{name} you have been placed in")
+
+def third_floor():
+
+def dark_forest():
+
+def fluffy_dog():
+
+def hagrids_hut():
+
+def hospital_wing():
+
+def peeves_poltergeist():
+
+def prof_dumbledore():
+
+def game_lost():
+
+def game_win():
 
 
 
-# Dormitories options
+
+
