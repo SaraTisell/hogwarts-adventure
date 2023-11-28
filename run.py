36a34aac61cf4c8c34080483b6f0e767a36ad7c1
Sara Tisell <Sara.Tisell@hotmail.com>
2023-11-28 15:39:20 +0000

    Add description to start game

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Description start game
+print("Welcome to Hogwarts Adventure")
+print("If you are ready to play the game please enter your name")
+name = input()
+print("Thank you, lets play and have fun!")
+print()
+print(f"Dear, {name}\nYou have been accepted to Hogwarts School for Witchcraft and Wizardry")
+print("It is time to place you in the right dormitory, our four dormitories are:\nGriffindor\nRavenclaw\nHufflepuff\nand\nSlytherin")
+
+# Dormitories options
