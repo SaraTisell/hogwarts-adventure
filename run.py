24e06d9b1220213a66f178536e403e1373d6c7d1
Sara Tisell <Sara.Tisell@hotmail.com>
2023-11-30 09:47:41 +0000

    Update select_dormitory, add challenge description

--- run.py
+++ run.py
@@ -24,34 +24,35 @@ def select_dormitory():
     print("We need to know your strenghts to make sure to put you in the right dormitory.\nPlease choose one of these four skills that best describes you greates strength! ")
     for dormitory_strength in dormitories.values():
         print(dormitory_strength)
-    #print(Please write your selected strength)
-    userDormitoryStrength = ""
+    userDormitoryStrength = input("Please write your selected strength:\n").capitalize()
     while userDormitoryStrength not in dormitories.values():
         print("Strengths: Bravery, Curiosity, Loyalty, Ambition")
-        userDormitoryStrength = input("Please write your selected strength:\n").capitalize()
+        userDormitoryStrength = input("Please write your selected strength:\n").capitalize()    
     
     userDormitory = dormitories.get(userDormitoryStrength)
-        
-select_dormitory()
-
-# Challenge description
-
-print((f"{name} you have been placed in {userDormitory}, which will suit you perfectly based on your skill {userDormitoryStrength}!\n"
+    
+    # Challenge description
+    print((f"{name} you have been placed in {userDormitory} , which will suit you perfectly based on your skill {userDormitoryStrength}!\n"
 "We have a very important mission for you, we need you to find the Philosopher's stone before it is to late!\n"
 "Find the stone while you explore the magic within Hogwarts, but be careful, challenges will come in your way.\n"
-f"Use your skill {userDormitoryStrength} well, and we will meet again in the end."
+f"Use your skill {userDormitoryStrength} well, and we will meet again in the end.\n"
 f"The best of luck to you {name}"))
 
+
+
+
+
+select_dormitory()
 """
 Function structure borrowed from https://www.makeuseof.com/python-text-adventure-game-create/
 """
-"""
+
 def start_exploring():
     option = ["1", "2"]
     print(("It is time to begin your jorney to find the Philosopher's stone, you can either go to the Third Floor or the Dark Forest\n"
     "Select 1 for Third Floor\n"
     "Selecet 2 for Dark Forest "))
-    userInput = ""
+    userInput = input()
     while userInput not in option:
         print("Please selecet 1 or 2")
         userInput = input()
@@ -64,7 +65,6 @@ def start_exploring():
 
 start_exploring()
 
-
 def third_floor():
     option = ["1", "2"]
     print(("The Third Floor is a higly forbidden area as very dangerous creatures roam here.. And yet here you are..\n"
@@ -72,7 +72,7 @@ def third_floor():
     "Take action now! You can either Hide or Run\n"
     "Select 1 to Run\n"
     "Select 2 to Hide"))
-    userInput = ""
+    userInput = input()
     while userInput not in option:
         print("Please selecet 1 or 2")
         userInput = input()
@@ -82,6 +82,9 @@ def third_floor():
             fluffy_dog()
         else:
             print("Please select 1 to Run or 2 to Hide")
+
+third_floor()
+"""
 
 def dark_forest():
 
@@ -100,7 +103,7 @@ def game_lost():
 def game_win():
 
 
-
+select_dormitory()
 
 """
 
